79f667017ab9065ffa6691ce8923af2d43260c1a
Ilari Meuronen <ilari@webflow.com>
2020-03-21 17:47:52 +0200

    Inital context and task format and adding

--- src/App.js
+++ src/App.js
@@ -6,31 +6,100 @@ import ChatIcon from "@material-ui/icons/Chat";
 import Chat from "./chat/Chat";
 import MainPage from "./MainPage";
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Link to="/home">
-          <AccountBalanceIcon />
-        </Link>
+const AppContext = React.createContext();
 
-        <Link to="/chat">
-          <ChatIcon />
-        </Link>
-      </div>
-      <Switch>
-        <Route path="/profile">
-          <h2>Profile</h2>
-        </Route>
-        <Route path="/home">
-          <MainPage />
-        </Route>
-        <Route path="/chat">
-          <Chat />
-        </Route>
-      </Switch>
-    </Router>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.addTask = task => {
+      this.setState(state => ({
+        tasks: [...state.tasks, task]
+      }));
+    };
+
+    this.state = {
+      user: {
+        name: "John Smith",
+        location: "00100",
+        mummos: [
+          {
+            location: "mummos home",
+            name: "Tyyne"
+          }
+        ]
+      },
+      tasks: [
+        {
+          id: 0,
+          owner: "John Smith",
+          created: new Date(),
+          type: {
+            type: "store",
+            items: ["milk", "bread", "beer"]
+          },
+          location: "Hevosenkengänkuja 2, 11011 Helsinki",
+          status: "closed"
+        },
+        {
+          id: 1,
+          owner: "John Smith",
+          created: new Date(),
+          type: {
+            type: "store",
+            items: ["milk", "bread", "beer"]
+          },
+          location: "Hevosenkengänkuja 2, 11011 Helsinki",
+          status: "open"
+        },
+        {
+          id: 13,
+          owner: "Tyyne",
+          created: new Date(),
+          type: {
+            type: "apotechary",
+            items: ["burana 600mg"]
+          },
+          location: "Temppelikatu 17, 00100 Helsinki",
+          status: "open"
+        }
+      ],
+      addTask: this.addTask
+    };
+  }
+
+  render() {
+    return (
+      <AppContext.Provider value={this.state}>
+        <Router>
+          <div>
+            <Link to="/home">
+              <AccountBalanceIcon />
+            </Link>
+
+            <Link to="/chat">
+              <ChatIcon />
+            </Link>
+          </div>
+          <Switch>
+            <Route path="/profile">
+              <h2>Profile</h2>
+            </Route>
+            <Route path="/home">
+              <AppContext.Consumer>
+                {({ tasks, addTask }) => (
+                  <MainPage tasks={tasks} addTask={addTask} />
+                )}
+              </AppContext.Consumer>
+            </Route>
+            <Route path="/chat">
+              <Chat />
+            </Route>
+          </Switch>
+        </Router>
+      </AppContext.Provider>
+    );
+  }
 }
 
 export default App;
--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -7,35 +7,46 @@ import {
   useParams
 } from "react-router-dom";
 
-const tasks = [
-  { id: 1, title: "A great task" },
-  { id: 3, title: "A mediocre task" }
-];
+const newTask = {
+  id: 9,
+  owner: "Pasha",
+  created: new Date(),
+  type: {
+    type: "store",
+    items: ["milk", "bread", "beer"]
+  },
+  location: "Home address",
+  status: "open"
+};
 
-const MainPage = () => {
+const MainPage = ({ tasks, addTask }) => {
   let { url, path } = useRouteMatch();
   return (
     <div>
       <h2>Tasks</h2>
 
       <ul>
-        {tasks.map(task => (
-          <li>
-            <Link to={`${url}/${task.id}`}>{task.title}</Link>
-          </li>
-        ))}
+        {tasks
+          .filter(t => t.status === "open")
+          .map((task, idx) => (
+            <li key={idx}>
+              <Link to={`${url}/${task.id}`}>
+                {task.type.type} in {task.location}
+              </Link>
+            </li>
+          ))}
       </ul>
-
+      <button onClick={() => addTask(newTask)}>new task</button>
       <Switch>
         <Route path={`${path}/:taskId`}>
-          <Task />
+          <Task tasks={tasks} />
         </Route>
       </Switch>
     </div>
   );
 };
 
-const Task = () => {
+const Task = ({ tasks }) => {
   let { taskId } = useParams();
   const task = tasks.find(t => String(t.id) === taskId);
   if (!task) return null;
@@ -48,7 +59,9 @@ const Task = () => {
         borderRadius: "5px"
       }}
     >
-      <h1>{task.title}</h1>
+      <h1>
+        Get {task.type.type} for {task.owner}
+      </h1>
       <h3>Requested task ID: {task.id}</h3>
     </div>
   );
