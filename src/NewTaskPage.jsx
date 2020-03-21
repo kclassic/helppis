14071931d539bb7a072c4b520a9487b9bf7efd75
Pashah <miika.kal@gmail.com>
2020-03-22 00:08:45 +0200

    Prettier ran

--- src/NewTaskPage.jsx
+++ src/NewTaskPage.jsx
@@ -10,8 +10,8 @@ import QueryBuilderIcon from "@material-ui/icons/QueryBuilder";
 import UpdateIcon from "@material-ui/icons/Update";
 import DateRangeIcon from "@material-ui/icons/DateRange";
 import Button from "./components/BasicButton";
-import Chip from '@material-ui/core/Chip';
-import TextField from '@material-ui/core/TextField';
+import Chip from "@material-ui/core/Chip";
+import TextField from "@material-ui/core/TextField";
 
 const Title = styled("h1")``;
 const SubTitle = styled("h3")``;
@@ -68,19 +68,19 @@ const NewTask = ({ tasks, addTask }) => {
   const [submitted, setSubmitted] = React.useState(false);
   const [step, setStep] = React.useState(0);
   const [shopItem, setShopItem] = React.useState([
-    { key: 0, label: 'Banaaneita 1kg' },
-    { key: 1, label: 'Vessapaperia' }
+    { key: 0, label: "Banaaneita 1kg" },
+    { key: 1, label: "Vessapaperia" }
   ]);
 
-  const handleInput  = (text) => {
-      const newChip = { key: shopItem.length, label: text};
-      setShopItem([...shopItems, newChip]);
+  const handleInput = text => {
+    const newChip = { key: shopItem.length, label: text };
+    setShopItem([...shopItems, newChip]);
   };
 
   const handleDelete = chipToDelete => () => {
-      setShopItem(chips => chips.filter(chip => chip.key !== chipToDelete.key));
+    setShopItem(chips => chips.filter(chip => chip.key !== chipToDelete.key));
   };
-  const [text, setText] = React.useState('');
+  const [text, setText] = React.useState("");
 
   const submitRequest = () => {
     const task = {
@@ -100,9 +100,9 @@ const NewTask = ({ tasks, addTask }) => {
     setSubmitted(true);
   };
 
-  const goToStep = (nextStep) => {
+  const goToStep = nextStep => {
     setStep(nextStep);
-  }
+  };
 
   if (submitted) {
     return <Redirect to="/home" />;
@@ -110,78 +110,78 @@ const NewTask = ({ tasks, addTask }) => {
   return (
     <>
       <Title>Luo avunpyyntö</Title>
-      {step === 0 ? 
-      <>
-      <SubTitle>Valitse tarve</SubTitle>
-      {templates.map((t, idx) => (
-        <ThingContainer
-          key={idx}
-          active={template.id === t.id}
-          onClick={() => setTemplate(t)}
-        >
-          {React.createElement(t.icon, {})}
-          <span>{t.title}</span>
-        </ThingContainer>
-      ))}
-
-      <SubTitle>Avun tarvitsija</SubTitle>
-      {mummos.map((m, idx) => (
-        <ThingContainer
-          key={idx}
-          active={mummo.id === m.id}
-          onClick={() => setMummo(m)}
-        >
-          <PersonOutlineIcon />
-          <span>{m.name},</span>
-          <span>{m.age},</span>
-          <span>{m.location}</span>
-        </ThingContainer>
-      ))}
-
-      <SubTitle>Tarpeen kiireellisyys</SubTitle>
-      {timeOptions.map((opt, idx) => (
-        <ThingContainer
-          key={idx}
-          active={timeOption.id === opt.id}
-          onClick={() => setTimeOption(opt)}
-        >
-          {React.createElement(opt.icon, {})}
-          <span>{opt.title}</span>
-        </ThingContainer>
-      ))}
-      <Button onClick={() => goToStep(1)}>Seuraava</Button>
-      </>
-      : null }
-      {step === 1 ? 
+      {step === 0 ? (
         <>
-        <Button onClick={() => goToStep(0)}>Edellinen</Button>
-        <Title>Ostoslista</Title>
-        <TextField
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        onKeyPress={(ev) => {
-            if (ev.key === 'Enter') {
+          <SubTitle>Valitse tarve</SubTitle>
+          {templates.map((t, idx) => (
+            <ThingContainer
+              key={idx}
+              active={template.id === t.id}
+              onClick={() => setTemplate(t)}
+            >
+              {React.createElement(t.icon, {})}
+              <span>{t.title}</span>
+            </ThingContainer>
+          ))}
+
+          <SubTitle>Avun tarvitsija</SubTitle>
+          {mummos.map((m, idx) => (
+            <ThingContainer
+              key={idx}
+              active={mummo.id === m.id}
+              onClick={() => setMummo(m)}
+            >
+              <PersonOutlineIcon />
+              <span>{m.name},</span>
+              <span>{m.age},</span>
+              <span>{m.location}</span>
+            </ThingContainer>
+          ))}
+
+          <SubTitle>Tarpeen kiireellisyys</SubTitle>
+          {timeOptions.map((opt, idx) => (
+            <ThingContainer
+              key={idx}
+              active={timeOption.id === opt.id}
+              onClick={() => setTimeOption(opt)}
+            >
+              {React.createElement(opt.icon, {})}
+              <span>{opt.title}</span>
+            </ThingContainer>
+          ))}
+          <Button onClick={() => goToStep(1)}>Seuraava</Button>
+        </>
+      ) : null}
+      {step === 1 ? (
+        <>
+          <Button onClick={() => goToStep(0)}>Edellinen</Button>
+          <Title>Ostoslista</Title>
+          <TextField
+            value={text}
+            onChange={e => setText(e.target.value)}
+            onKeyPress={ev => {
+              if (ev.key === "Enter") {
                 ev.preventDefault();
                 handleInput(text);
-                setText('');
-            }
-        }}
-        />
-        <SubTitle>Ostokset</SubTitle>
-        {shopItem.map(data => {
+                setText("");
+              }
+            }}
+          />
+          <SubTitle>Ostokset</SubTitle>
+          {shopItem.map(data => {
             return (
-            <Chip
-            key={data.key}
-            label={data.label}
-            onDelete={handleDelete(data)}
-            variant="outlined"
-            color="primary"
-            />
-        );
-      })}
-      <Button onClick={submitRequest}>Lähetä avun tarve ilmoitus</Button>
-    </>
-      : null}
+              <Chip
+                key={data.key}
+                label={data.label}
+                onDelete={handleDelete(data)}
+                variant="outlined"
+                color="primary"
+              />
+            );
+          })}
+          <Button onClick={submitRequest}>Lähetä avun tarve ilmoitus</Button>
+        </>
+      ) : null}
     </>
   );
 };
